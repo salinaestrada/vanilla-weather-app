1b5fa9337da0c993523b3db0c6e838b9da53fa35
Salina E <sestrada@squarespace.com>
2023-03-07 16:46:45 -0600

    Added Humidity, Wind, and Feels Like API data
    Changed Precipitation to Feels Like because API did not have a Precipitation option

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 function displayTemperature(response) {
   console.log(response.data);
+  console.log(response.data.temperature.feels_like);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let conditionsElement = document.querySelector("#conditions");
+  let feelElement = document.querySelector("#feel");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   conditionsElement.innerHTML = response.data.condition.description;
+  feelElement.innerHTML = Math.round(response.data.temperature.feels_like);
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 let apiKey = "be8of109b6t500324a628a4f8a83394b";
 let apiUrl =
